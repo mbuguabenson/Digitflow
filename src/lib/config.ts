a28d70f4b40497e118ebf6c92100eb037b7280a4
benso@Benadesigns <mbuguabenson2020@gmail.com>
2026-08-31 20:18:22 +0300

    Revert PUBLIC_WS_URL to the new api endpoint

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 export const DERIV_APP_ID = '343aX4kokbJ7cI5C42ytV';
 export const DERIV_API_URL = 'https://api.derivws.com';
-export const PUBLIC_WS_URL = 'wss://ws.binaryws.com/websockets/v3?app_id=1089';
+export const PUBLIC_WS_URL = 'wss://api.derivws.com/trading/v1/options/ws/public';
 export const TOKEN_EXCHANGE_URL = 'https://umnsatnhehymerpwdpze.supabase.co/functions/v1/deriv-token-exchange';
 export const LOGIN_URL = 'https://digitflowhub.vercel.app';
 
